560cf76746142117344a23fc674cae08cc91f93a
mpl <mathieu.lonjaret@gmail.com>
2018-06-05 23:03:33 +0200

    gopherjs: repro for memleak on http GET

---
+++ gopherjs/gocode/main.go
@@ -0,0 +1,45 @@
+// this program demonstrates how fetches through gopherjs seem to be memory leaking.
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func main() {
+	js.Global.Set("gocode", map[string]interface{}{
+		"fetch": fetch,
+	})
+}
+
+const (
+	maxFetch = 1000
+	fetchURL = "http://localhost:8080/gocode.js"
+)
+
+var fetchInterval = 10 * time.Millisecond
+
+func fetch() {
+	go func() {
+		for i := 0; i < maxFetch; i++ {
+			time.Sleep(fetchInterval)
+			req, err := http.NewRequest("GET", fetchURL, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+}
